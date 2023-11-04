5235ef1498b8e941718aa188dcb4a12dbde1eb50
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-04 15:15:31 -0700

    Update linux_device_driver_kernel.c
    continue spi

--- linux_device_driver_kernel.c
+++ linux_device_driver_kernel.c
@@ -1,8 +1,12 @@
 -1)i2c using character device driver approach NOT platform approach
+    -The reason for using character device driver is need to return sth to the application layer as copy_to
   ->Lecture 22.3->https://www.bilibili.com/video/BV1fJ411i7PB?p=77&vd_source=352d86e561ef82fd069e1251bfae8933
     -(19:15)
   -Lecture 22.4->https://www.bilibili.com/video/BV1fJ411i7PB?p=78&vd_source=352d86e561ef82fd069e1251bfae8933
   -Lecture 22.5->
+  -Lecture 22.6
+->spi 
+  -Lecture 23.1
 -
   -
   -
