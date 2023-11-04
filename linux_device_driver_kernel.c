eef82aa24aa678568f9857896f14821ce092805b
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-04 02:03:46 -0700

    Update linux_device_driver_kernel.c
    update on i2c character approach

--- linux_device_driver_kernel.c
+++ linux_device_driver_kernel.c
@@ -1,4 +1,4 @@
--1)i2c
+-1)i2c using character device driver approach NOT platform approach
   ->Lecture 22.3->https://www.bilibili.com/video/BV1fJ411i7PB?p=77&vd_source=352d86e561ef82fd069e1251bfae8933
     -(19:15)
   -Lecture 22.4->https://www.bilibili.com/video/BV1fJ411i7PB?p=78&vd_source=352d86e561ef82fd069e1251bfae8933
