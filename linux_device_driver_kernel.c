55ff057f4e761dafe71b5790de6b891545e7d660
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-04 02:43:27 -0700

    Update linux_device_driver_kernel.c
    lecture 22.5

--- linux_device_driver_kernel.c
+++ linux_device_driver_kernel.c
@@ -2,6 +2,7 @@
   ->Lecture 22.3->https://www.bilibili.com/video/BV1fJ411i7PB?p=77&vd_source=352d86e561ef82fd069e1251bfae8933
     -(19:15)
   -Lecture 22.4->https://www.bilibili.com/video/BV1fJ411i7PB?p=78&vd_source=352d86e561ef82fd069e1251bfae8933
+  -Lecture 22.5->
 -
   -
   -
