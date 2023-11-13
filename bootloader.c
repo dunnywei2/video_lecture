9c9509ec54b47f79fb8452d475e187eaf0f74161
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-13 00:17:40 -0800

    Update bootloader.c
    eufi

--- bootloader.c
+++ bootloader.c
@@ -2,4 +2,7 @@ EUFI->
   https://www.youtube.com/watch?v=BBUPc0aO5lw&pp=ygUPZXVmaSBib290IGxpbnV4
 
 U-BOOT->
-  
+
+Search terms for->"arm uefi"
+ ->https://www.youtube.com/watch?v=X-re0BZW6pM&pp=ygUIYXJtIHVlZmk%3D
+ ->https://www.youtube.com/watch?v=aHPZ94io_OE&pp=ygUIYXJtIHVlZmk%3D
