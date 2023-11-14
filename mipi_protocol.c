35f2f56c4c05b7967f121651e32cd3a722b06523
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-13 21:12:16 -0800

    Update mipi_protocol.c
    mip csi protocol

--- mipi_protocol.c
+++ mipi_protocol.c
@@ -2,4 +2,5 @@
   ->https://www.bilibili.com/video/BV19m4y1v7hk/?spm_id_from=333.337.search-card.all.click
    ->fpga->https://www.bilibili.com/video/BV188411o7bL/?spm_id_from=333.337.search-card.all.click
   ->linux->https://www.youtube.com/watch?v=UG7tMZM24UU&pp=ygUKbWlwaSBsaW51eA%3D%3D
-  
+2-> MIPI CSI 驱动
+     ->https://www.bilibili.com/video/BV1t8411q7rX/?spm_id_from=333.337.search-card.all.click
