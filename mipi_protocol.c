ce26164ddc9ff5d47adf3f723721cd9a454576c9
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-13 21:08:12 -0800

    Create mipi_protocol.c
    mipi linux driver

---
+++ mipi_protocol.c
@@ -0,0 +1,5 @@
+1->mipi驱动 or I3C
+  ->https://www.bilibili.com/video/BV19m4y1v7hk/?spm_id_from=333.337.search-card.all.click
+   ->fpga->https://www.bilibili.com/video/BV188411o7bL/?spm_id_from=333.337.search-card.all.click
+  ->linux->https://www.youtube.com/watch?v=UG7tMZM24UU&pp=ygUKbWlwaSBsaW51eA%3D%3D
+  
