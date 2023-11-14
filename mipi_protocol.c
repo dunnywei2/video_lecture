a4b7b60a6a6e51959e81e0b3d09a7618499d9fc6
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-13 22:49:49 -0800

    Update mipi_protocol.c
    update i3c

--- mipi_protocol.c
+++ mipi_protocol.c
@@ -6,3 +6,7 @@
      ->https://www.bilibili.com/video/BV1t8411q7rX/?spm_id_from=333.337.search-card.all.click
 3->I3C->
   ->https://www.youtube.com/watch?v=g3TBNHec5Ec
+  ->I3C training TI->https://www.youtube.com/watch?v=oESuzmErm0I
+  ->I3C linux->
+  a)https://www.youtube.com/watch?v=lMncnFsTJhA&pp=ygUJaTNjIGxpbnV4
+  b)https://www.youtube.com/watch?v=AtgrDMrCcVI&list=PLLmhTu6KRAa1mPwFgkBMHfveBGlrS1HAJ
