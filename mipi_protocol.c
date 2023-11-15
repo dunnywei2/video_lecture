9491b53451f25f5517e6870876e728af2aa765e4
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2023-11-14 16:30:08 -0800

    Update mipi_protocol.c
    mipi csi driver update

--- mipi_protocol.c
+++ mipi_protocol.c
@@ -2,7 +2,7 @@
   ->https://www.bilibili.com/video/BV19m4y1v7hk/?spm_id_from=333.337.search-card.all.click
    ->fpga->https://www.bilibili.com/video/BV188411o7bL/?spm_id_from=333.337.search-card.all.click
   ->linux->https://www.youtube.com/watch?v=UG7tMZM24UU&pp=ygUKbWlwaSBsaW51eA%3D%3D
-2-> MIPI CSI 驱动
+2-> MIPI CSI 驱动 or mipi csi-2 linux
      ->https://www.bilibili.com/video/BV1t8411q7rX/?spm_id_from=333.337.search-card.all.click
 3->I3C->
   ->https://www.youtube.com/watch?v=g3TBNHec5Ec
