5627f496abbea1098a3621ca9671e4f7f1f9f692
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2024-03-12 04:25:27 -0700

    Update linux_device_driver_kernel.c
    Lecture_12 (25:58) interrupt w.r.t GIC

--- linux_device_driver_kernel.c
+++ linux_device_driver_kernel.c
@@ -21,7 +21,7 @@
 ->Lecture 12->
     ->POLLIN and POLLOUT for sound card (2:59)
     ->ret!=POLL_IN then ret!=POLL_OUT (6:45)
-    -
+    ->Hardware interrupt and virtual(Software) interrupt with GIC (25:58)
     -
 -
 -
