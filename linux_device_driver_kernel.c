558fc2879aa2d47ebaf05e2143a350052753f63a
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2024-03-11 22:01:54 -0700

    Update linux_device_driver_kernel.c
    Lecture 12->sound card (2:59)

--- linux_device_driver_kernel.c
+++ linux_device_driver_kernel.c
@@ -18,7 +18,28 @@
     -blocking interrrupt and wake up (38:54)
     -LInux 内核源代码情景分析 (1:43:31)
     -signal handling != sempahore as 信号量 (2:03:44)
+->Lecture 12->
+    ->POLLIN and POLLOUT for sound card (2:59)
+    -
+    -
+    -
 -
+-
+-
+    -
+    -
+    -
+    -
+-
+-
+-
+-
+    -
+    -
+    -
+    -
+
+    -
 -
 -
 -
